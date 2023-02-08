20aeafe021b3b00c41cc7bf4b3e8d40922ce4b13
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-08 00:27:34 -0300

    Page home

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Movie } from './types/Movie';
+import filmImage from './assets/tv.png'
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -20,17 +21,35 @@ function App() {
   }
 
   return (
-    <div className="App flex justify-center items-center flex-col">
+    <div className="App flex justify-center items-center flex-col min-h-screen bg-stone-900">
 
-      <header className='bg-stone-800 h-16 w-full flex items-center justify-between px-5'>
-        <h1 className='text-gray-100 text-4xl'>DevFlix</h1>
+     <div className='container'>
+     <header className='bg-stone-800 h-16 w-full flex items-center justify-between px-5 fixed top-0'>
+        <h1 className='text-gray-200 text-4xl font-bold cursor-pointer'>DevFlix</h1>
+        <div className='flex gap-5 cursor-pointer	text-gray-300 uppercase'>
+          <a href="#" className='hover:text-white'>ingressos</a>
+          <a href="#" className='hover:text-white'>filmes</a>
+          <a href="#" className='hover:text-white'>combo</a>
+        </div>
       </header>
 
+      <div className='flex mt-20'>
+        <div className='px-16 w-full flex flex-col gap-6'>
+          <h2 className='font-bold text-slate-200 text-7xl font-sans uppercase'>Que tal um cineminha ?</h2>
+          <p className='mb-10 font-semibold text-xl text-slate-300'>Confira agora filmes que estão em cartaz, e escolha qual será a sua próxima aventura... Ou romance, você quem decide!</p>
 
-
-        <button className={`${display} bg-blue-500 p-2 rounded`} onClick={loadMovies}>
-          Carregar Filmes
-        </button>
+          <button className={`${display} bg-cyan-700 p-2 rounded uppercase text-gray-300
+            hover:bg-cyan-500 hover:text-white duration-300 py-3`} 
+            onClick={loadMovies}>
+            filmes em cartaz
+          </button>
+        </div>
+        
+        <div className='w-full flex items-center justify-center'>
+          <img src={filmImage} alt="Imagem Filme" width={400}/>
+        </div>
+      </div>
+     </div>
         
        {loading &&
           <div className='px-5 py-2 rounded-xl text-gray-700 bg-gradient-to-r from-amber-300 via-amber-500 to-amber-600'>Carregando...</div>
